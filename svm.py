a68ebfea426bf18e5908927ffd8e1a3b3a2f2822
Trevor Lutzow <trevorlutzow@gmail.com>
2015-04-23 14:02:37 -0400

    Created svm.py file

---
+++ svm.py
@@ -0,0 +1,16 @@
+__author__ = 'trevorlutzow'
+
+import os, struct
+from array import array
+
+digit_images = os.path.join(".", 'digits/train-images')
+digit_labels = os.path.join(".", 'digits/train-labels')
+
+def load_mnist ():
+  with open(digit_labels, 'rb') as f:
+    labels = array("b", f.read())
+  with open(digit_images, 'rb') as f:
+    images = array("B", f.read())
+  return images, labels
+
+images, labels = load_mnist()
